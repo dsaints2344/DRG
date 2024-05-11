86d0f67765247d627700ef6811bc6c6103d8d1c1
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-05-10 23:57:12 -0400

    Creating APRDRGV36 DTO

---
+++ APRDRGV36DTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class APRDRGV36DTO
+{
+    public required string APRDRG { get; set; }  // APR DRG code
+    public int SOIScore { get; set; } // Severity of illness score
+    public required string CombinedSOI { get; set; }  // APR DRG-SOI
+    public required string Description { get; set; }  // DRG Description
+    public decimal? V36RelativeWeight { get; set; }
+    public double? MLOS { get; set; } // Mean Length of stay
+    public int? DayOutlierThreshold { get; set; }
+}
