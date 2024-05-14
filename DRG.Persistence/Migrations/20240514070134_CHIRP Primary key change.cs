3783bb2b24bcad2f3c0cf3d7d5ad2aa0b4f94db0
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-05-14 03:11:38 -0400

    Create 20240514070134_CHIRP Primary key change.cs

---
+++ DRG.Persistence/Migrations/20240514070134_CHIRP Primary key change.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DRG.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CHIRPPrimarykeychange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hospitals_CHIRPHospitals_TIN",
+                table: "Hospitals");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CHIRPHospitals",
+                table: "CHIRPHospitals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TIN",
+                table: "CHIRPHospitals",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CHIRPNPI",
+                table: "CHIRPHospitals",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CHIRPHospitals",
+                table: "CHIRPHospitals",
+                column: "CHIRPNPI");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hospitals_CHIRPHospitals_TIN",
+                table: "Hospitals",
+                column: "TIN",
+                principalTable: "CHIRPHospitals",
+                principalColumn: "CHIRPNPI");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hospitals_CHIRPHospitals_TIN",
+                table: "Hospitals");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CHIRPHospitals",
+                table: "CHIRPHospitals");
+
+            migrationBuilder.DropColumn(
+                name: "CHIRPNPI",
+                table: "CHIRPHospitals");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TIN",
+                table: "CHIRPHospitals",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CHIRPHospitals",
+                table: "CHIRPHospitals",
+                column: "TIN");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hospitals_CHIRPHospitals_TIN",
+                table: "Hospitals",
+                column: "TIN",
+                principalTable: "CHIRPHospitals",
+                principalColumn: "TIN");
+        }
+    }
+}
