381d586a5ef3609e7434816ef546631f3c7c81d4
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2024-05-14 01:51:34 -0400

    Update CHIRPHospitalDTO.cs

--- DRG.APPLICATION/DTOs/CHIRPHospitalDTO.cs
+++ DRG.APPLICATION/DTOs/CHIRPHospitalDTO.cs
@@ -7,6 +7,9 @@
         public required string TIN { get; set; }
         public required string SDA { get; set; }
         public required string CHIRPCLASS { get; set; }
+        public required decimal ACIAOP { get; set; }
+        public required decimal ACIAIP { get; set; }
+        public required decimal TotalACIA { get; set; }
         public required decimal IP { get; set; }
         public required decimal OP { get; set; }
         public required decimal Total { get; set; }
